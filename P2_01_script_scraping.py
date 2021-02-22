4e1d9c56e03fe6534885141e65d6336f5ef6b9d2
Quentin Chevallier <quentinchevallier1@gmail.com>
2021-02-22 22:53:49 +0100

    P2_01_script_scraping_v3 Recovery of data from all the books, write them in the CSV files corresponding to their category then download their visuel.

--- P2_01_script_scraping.py
+++ P2_01_script_scraping.py
@@ -1,74 +1,90 @@
 import requests
+from urllib import request
 from bs4 import BeautifulSoup
+import os
 
-#Creation/ouverture du fichier csv en ecriture 
-with open('Book_informations.csv', 'w', encoding='utf-8') as book_informations:
-    book_informations.write('product_page_url'+ ',' +'universal_product_code'+ ',' + 'title'+ ',' +'price_including_tax'+ ',' +'price_excluding_tax'+ ',' +'number_available'+ ',' +'product_desciption'+
-     ',' +'category'+ ',' +'review_rating'+ ',' +'image_url'+ '\n')
+count = 0
+#Initiale URL
+url = 'http://books.toscrape.com/index.html'
+reponse_url = requests.get(url)
 
-    #Requete url categorie et test reponse
-    url_category = 'http://books.toscrape.com/catalogue/category/books/travel_2/index.html'
-    reponse_category = requests.get(url_category)
+#Source code recovery  
+if reponse_url.ok:
+    soup_url = BeautifulSoup(reponse_url.text, features='html.parser')
+    url_category_name = []
+    url_category_nb = soup_url.find('ul', {'class': 'nav nav-list'}).find('ul').findAll('a')
+    url_category = []
+    i = 0
+ 
+    while i < len(url_category_nb):
+        url_category_name.append(soup_url.find('ul', {'class': 'nav nav-list'}).find('ul').findAll('a')[i].text.strip())
+        #Creation of all CSV files and writing column names
+        with open(url_category_name[i] + '.csv', 'w', encoding='utf-8') as book_informations:
+            book_informations.write('product_page_url'+ ' , ' +'universal_product_code'+ ' , ' + 'title'+ ' , ' +'price_including_tax'+ ' , ' +'price_excluding_tax'+ ' , ' 
+            + 'number_available'+ ' , ' + 'product_desciption'+ ' , ' + 'category'+ ' , ' + 'review_rating'+ ' , ' +'image_url'+ '\n')
+        #Retrieving the url of all categories
+        x = url_category_nb[i]['href']
+        url_category.append('http://books.toscrape.com/' + x )
+        i += 1
+
+for link in url_category:
+    reponse_category = requests.get(link)
+
     if reponse_category.ok: 
-
-        #Recuperation code source url, stockage liens livres 
+        #Retrieving the source code and searching for the URL of the books in each category
         soup_category = BeautifulSoup(reponse_category.text, features='html.parser')
-        #Test pagination
         nb_book = soup_category.findAll('strong')[1].text
         product_page_url = []
+
+        #Test number of pages
         if int(nb_book) <= 20:
             h3s = soup_category.findAll('h3')
+
             for h3 in h3s:
                 a = h3.find('a')
                 a = a['href'].replace('../../..', 'http://books.toscrape.com/catalogue')
                 product_page_url.append(a)
-                print(a)
+
         else:
             nb_page = soup_category.find('li', {'class': 'current'}).text.replace('Page 1 of ', '').strip()
-            print(nb_page)
             i = 1
+
             while i <= int(nb_page):
                 i_str = 'page-' + str(i)
                 i += 1
-                print(i_str)
-                url_category_page_x = url_category.replace('index', i_str)
-                print(url_category_page_x, '\n')
+                url_category_page_x = link.replace('index', i_str)
                 reponse_category = requests.get(url_category_page_x)
+
                 if reponse_category.ok:
                     soup_category = BeautifulSoup(reponse_category.text, features='html.parser')
                     h3s = soup_category.findAll('h3')
+
                     for h3 in h3s:
                         a = h3.find('a')
                         a = a['href'].replace('../../..', 'http://books.toscrape.com/catalogue')
                         product_page_url.append(a)
-                        print(a)
-
-        #Total livres 
-        print('\n', len(product_page_url))
-
         i = 0
-        while i < len(product_page_url):
-            #Requete url livre et test reponse
-            reponse_book = requests.get(product_page_url[i])
-            if reponse_book.ok:
-                print(reponse_book, '\t', i+1)
 
-                #Recuperation code source url
+        while i < len(product_page_url):
+            reponse_book = requests.get(product_page_url[i])
+
+            #Retrieving the source code of each book
+            if reponse_book.ok:
                 soup_book = BeautifulSoup(reponse_book.text, features='html.parser')
-                #Recuperation des donnees dans le code source
+                #Retrieving books data
                 title = soup_book.find('div', {'class' : 'col-sm-6 product_main'}).find('h1').text
                 universal_product_code = soup_book.find('table', {'class': 'table table-striped'}).findAll('td')[0].text
                 price_including_tax = soup_book.find('table', {'class': 'table table-striped'}).findAll('td')[3].text
                 price_excluding_tax = soup_book.find('table', {'class': 'table table-striped'}).findAll('td')[2].text
                 number_available = soup_book.find('table', {'class': 'table table-striped'}).findAll('td')[5].text.replace('In stock (','').replace('available)','')
-                product_description = soup_book.findAll('p')[3].text
-                category = soup_book.find('ul', {'class': 'breadcrumb'}).findAll('li')[2].find('a').text
-
-                #Convertion str en note sur 5
+                product_description = soup_book.findAll('p')[3].text.strip()
+                category = soup_book.find('ul', {'class': 'breadcrumb'}).findAll('a')[2].text
+                #Conversion str to note per 5
                 review_rating_str = soup_book.find('div', {'class': 'col-sm-6 product_main'}).findAll('p')[2]
                 review_rating_str = str(review_rating_str['class'])
                 review_rating_str = review_rating_str.replace("['star-rating', '", '').replace("']", '')
                 review_rating = '0'
+                
                 if review_rating_str == 'Five':
                     review_rating = '5/5'
                 elif review_rating_str == 'Four':
@@ -79,14 +95,20 @@ with open('Book_informations.csv', 'w', encoding='utf-8') as book_informations:
                     review_rating = '2/5'
                 elif review_rating_str == 'One':
                     review_rating = '1/5'
-
                 image_url = soup_book.find('div', {'class': 'item active'}).find('img')
                 image_url = str(image_url['src'])
                 image_url = image_url.replace('../../', 'http://books.toscrape.com/')
+                count += 1
+                #Download image from book in a folder
+                path = os.getcwd() + '\\Image\\'
+                os.makedirs(path, exist_ok=True)
+                final_title = path + str(count) + ' ' + title.replace(':', '').replace('/', ' ').replace('\\', '').replace('"',"'").replace('*', '').replace('?','').replace('|', '').replace('<','').replace('>','') + '.jpg'
+                if len(final_title) >= 256:
+                    final_title = final_title[:250]  + '.jpg'
+                request.urlretrieve(image_url, final_title)
 
-                #Ecriture des donees dans le fichier csv
-                book_informations.write(product_page_url[i] + ',' + universal_product_code + ',' + title + ',' + price_including_tax + ',' + price_excluding_tax + ',' +
-                    number_available + ',' + product_description + ',' + category + ',' + review_rating + ',' + image_url + '\n')
-                i += 1
-            else:
-                print('Page introuvale.')
+                #Writing data in the right CSV files 
+                with open(category + '.csv', 'a', encoding='utf-8') as book_informations:
+                    book_informations.write(product_page_url[i] + ' , ' + universal_product_code + ' , ' + title + ' , ' + price_including_tax + ' , ' + price_excluding_tax + ' , ' +
+                        number_available + ' , ' + product_description + ' , ' + category + ' , ' + review_rating + ' , ' + image_url + '\n')
+                    i += 1
--- README.md
+++ README.md
@@ -1,22 +1,23 @@
 # P2_01_script_scraping
 
-P2_01_script_scraping is a Python script who catches
+P2_01_script_scraping is a Python script wich catches
 many information about all books on 
 the site 'http://books.toscrape.com/index.html'
 to write them to a CSV file.
 
 ## Installation
 
-You must use the package manager [pip](https://pip.pypa.io/en/stable/)
-and install the following packages :
+Open your terminal in administrator mode and do 
+the following steps :
 ```bash
-pip install bs4
-pip install requests 
+cd ..\P2_01_script_scraping
+python -m venv env
+pip install -a requierments.txt
 ```
 
 ## Usage
 
-After all, just run the script.
+After this, just run the script.
 This may take a few minutes.
 
 ## Author
